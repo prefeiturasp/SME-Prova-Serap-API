36948c8d0d6f809ede004c18c6c81c389e3ae984
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-08-06 11:24:18 -0300

    Endpoint Listar Provas
    Endpoint mockado de listagem de provas para area do estudante

---
+++ src/SME.SERAp.Prova.Api/Controllers/ProvaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.SERAp.Prova.Aplicacao;
+using SME.SERAp.Prova.Infra;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/provas")]
+    public class ProvaController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<ObterProvasRetornoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Authorize("Bearer")]
+        public async Task<IActionResult> ObterProvas([FromServices] IObterProvasAreaEstudanteUseCase obterProvasAreaEstudanteUseCase)
+        {
+            return Ok(await obterProvasAreaEstudanteUseCase.Executar());
+        }
+    }
+}
