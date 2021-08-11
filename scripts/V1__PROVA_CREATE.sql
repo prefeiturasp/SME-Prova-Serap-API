8136ba6c48f27cac9687de6be14f63352ef06c23
thiiagofernando <thiago.fernando@msn.com>
2021-08-11 19:23:47 -0300

    Add Index
    AB#47079

--- scripts/V1__PROVA_CREATE.sql
+++ scripts/V1__PROVA_CREATE.sql
@@ -7,3 +7,5 @@ create table if not exists public.prova
 	datafim timestamp without time zone not null,
 	CONSTRAINT prova_pk PRIMARY KEY (id)
 );
+create index if not exists prova_datainicio_idx ON public.prova (datainicio);
+create index if not exists prova_datafim_idx ON public.prova (datafim);
