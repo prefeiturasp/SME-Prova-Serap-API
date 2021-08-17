fc24774741832a28ebd2cbacde231450063d5c3a
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-08-16 21:33:48 -0300

    teste

--- src/SME.SERAp.Prova.Aplicacao/UseCase/ObterProvasAreaEstudanteUseCase.cs
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/ObterProvasAreaEstudanteUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Sentry;
 using SME.SERAp.Prova.Infra;
 using SME.SERAp.Prova.Infra.Exceptions;
 using System;
@@ -22,6 +23,7 @@ namespace SME.SERAp.Prova.Aplicacao
             if (string.IsNullOrEmpty(alunoLogadoAno))
                 throw new NegocioException("Ano do aluno logado não localizado");
 
+            SentrySdk.CaptureMessage($"ano:{alunoLogadoAno} - data:{DateTime.Now}");
             var provas = await mediator.Send(new ObterProvasPorAnoQuery(int.Parse(alunoLogadoAno), DateTime.Now));
             if (provas.Any())
             {
@@ -31,7 +33,7 @@ namespace SME.SERAp.Prova.Aplicacao
                 {
                     provasParaRetornar.Add(new ObterProvasRetornoDto(prova.Descricao, prova.TotalItens, prova.Inicio, prova.Fim));
                 }
-                
+
                 return provasParaRetornar;
             }
             else return default;
