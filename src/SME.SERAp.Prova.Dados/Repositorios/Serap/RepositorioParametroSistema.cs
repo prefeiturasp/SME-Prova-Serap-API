b4fd8b4bdf93c0d08545989d14b3d927798a72b5
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-09-27 10:22:48 -0300

    ajustes nos parametros do sistema

---
+++ src/SME.SERAp.Prova.Dados/Repositorios/Serap/RepositorioParametroSistema.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using SME.SERAp.Prova.Dominio;
+using SME.SERAp.Prova.Infra;
+using SME.SERAp.Prova.Infra.EnvironmentVariables;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Dados
+{
+    public class RepositorioParametroSistema : RepositorioBase<Dominio.ParametroSistema>, IRepositorioParametroSistema
+    {
+        public RepositorioParametroSistema(ConnectionStringOptions connectionStringOptions) : base(connectionStringOptions)
+        {
+
+        }
+
+        public async Task<ParametroSistema> ObterPorTipoEAno(int tipo, int ano)
+        {
+            using var conn = ObterConexao();
+            try
+            {
+                var query = @"select * from parametro_sistema ps 
+                                where ps.ano = @ano and ps.tipo = @tipo";
+
+                return await conn.QueryFirstOrDefaultAsync<ParametroSistema>(query, new { tipo, ano });
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+        }
+    }
+}
---
+++ src/SME.SERAp.Prova.Dominio/Entidades/ParametroSistema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SME.SERAp.Prova.Dominio
+{
+    public class ParametroSistema : EntidadeBase
+    {
+       
+        public int? Ano { get; set; }
+        public bool Ativo { get; set; }
+        public string Descricao { get; set; }
+        public string Nome { get; set; }
+        public TipoParametroSistema Tipo { get; set; }
+        public string Valor { get; set; }
+
+        public ParametroSistema()
+        {
+
+        }
+
+        public ParametroSistema(int ano,bool ativo, string descricao, string nome, TipoParametroSistema tipo, string valor)
+        {
+            Ano = ano;
+            Ativo = ativo;
+            Descricao = descricao;
+            Nome = nome;
+            Tipo = tipo;
+            Valor = valor;
+    }
+    }
+}
