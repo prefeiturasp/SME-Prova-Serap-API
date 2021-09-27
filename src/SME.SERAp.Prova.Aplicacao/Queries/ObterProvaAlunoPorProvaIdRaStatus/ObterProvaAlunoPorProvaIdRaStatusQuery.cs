7c08d515fee72bfbe11dcc489ce87a53bb888667
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-09-27 10:41:02 -0300

    criada flag para provas finalizadas

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterProvaAlunoPorProvaIdRaStatus/ObterProvaAlunoPorProvaIdRaStatusQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dominio;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ObterProvaAlunoPorProvaIdRaStatusQuery : IRequest<ProvaAluno>
+    {
+        public ObterProvaAlunoPorProvaIdRaStatusQuery(long provaId, long alunoRa, int status)
+        {
+            ProvaId = provaId;
+            AlunoRa = alunoRa;
+            Status = status;
+        }
+
+        public long ProvaId { get; set; }
+        public long AlunoRa { get; set; }
+        public int Status { get; set; }
+    }
+}
--- src/SME.SERAp.Prova.Dados/Interfaces/IRepositorioProvaAluno.cs
+++ src/SME.SERAp.Prova.Dados/Interfaces/IRepositorioProvaAluno.cs
@@ -6,5 +6,6 @@ namespace SME.SERAp.Prova.Dados
     public interface IRepositorioProvaAluno : IRepositorioBase<ProvaAluno>
     {
         Task<ProvaAluno> ObterPorProvaIdRaAsync(long provaId, long alunoRa);
+        Task<ProvaAluno> ObterPorProvaIdRaStatusAsync(long provaId, long alunoRa, int status);
     }
 }
