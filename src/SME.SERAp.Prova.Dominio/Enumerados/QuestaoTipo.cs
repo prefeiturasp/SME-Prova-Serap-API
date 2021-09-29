298eb4315549f490122f385c03c297257201fe91
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-09-29 12:29:48 -0300

    Correção nos tipos de questões

--- src/SME.SERAp.Prova.Dominio/Enumerados/QuestaoTipo.cs
+++ src/SME.SERAp.Prova.Dominio/Enumerados/QuestaoTipo.cs
@@ -5,14 +5,16 @@ namespace SME.SERAp.Prova.Dominio
 {
     public enum QuestaoTipo
     {
-
         [Display(Name = "Não Cadastrado")]
         NaoCadastrado = 0,
 
-        [Display(Name = "Multipla escolha")]
-        MultiplaEscolha = 1,
+        [Display(Name = "Múltipla escolha 4 Alternativas")]
+        MultiplaEscolha4Alternativas = 1,
+
+        [Display(Name = "Múltipla escolha 5 Alternativas")]
+        MultiplaEscolha5Alternativas = 2,
 
         [Display(Name = "Resposta construída")]
-        RespostaConstruida = 2,
+        RespostaConstruida = 3,
     }
 }
