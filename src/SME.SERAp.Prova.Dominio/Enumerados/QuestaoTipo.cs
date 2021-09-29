cd1d7530396ac71a65fff2726c075b8cb7978b61
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-09-29 15:08:55 -0300

    atualizada nomenclatura

--- src/SME.SERAp.Prova.Dominio/Enumerados/QuestaoTipo.cs
+++ src/SME.SERAp.Prova.Dominio/Enumerados/QuestaoTipo.cs
@@ -5,13 +5,13 @@ namespace SME.SERAp.Prova.Dominio
 {
     public enum QuestaoTipo
     {
-        [Display(Name = "Não Cadastrado")]
+        [Display(Name = "Não cadastrado")]
         NaoCadastrado = 0,
 
-        [Display(Name = "Múltipla escolha 4 Alternativas")]
+        [Display(Name = "Múltipla escolha 4 alternativas")]
         MultiplaEscolha4Alternativas = 1,
 
-        [Display(Name = "Múltipla escolha 5 Alternativas")]
+        [Display(Name = "Múltipla escolha 5 alternativas")]
         MultiplaEscolha5Alternativas = 2,
 
         [Display(Name = "Resposta construída")]
