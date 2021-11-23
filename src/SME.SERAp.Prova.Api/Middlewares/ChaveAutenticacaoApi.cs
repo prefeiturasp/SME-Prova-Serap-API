ae8d1f2f49aa48d9d8f896f17aa400f2ae51e925
krisamcom <68442245+krisamcom@users.noreply.github.com>
2021-11-23 14:10:25 -0300

    feature/53835 - chave autenticação API

---
+++ src/SME.SERAp.Prova.Api/Middlewares/ChaveAutenticacaoApi.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Api.Middlewares
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ChaveAutenticacaoApi : Attribute, IAsyncActionFilter
+    {
+        private const string ChaveHeader = "chave-api";
+        private const string ChaveEnvironmentVariableName = "ChaveSerapProvaApi";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            string chaveApi = Environment.GetEnvironmentVariable(ChaveEnvironmentVariableName);
+            if (!context.HttpContext.Request.Headers.TryGetValue(ChaveHeader, out var chaveRecebida) || !chaveRecebida.Equals(chaveApi))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            await next();
+        }
+    }
+}
