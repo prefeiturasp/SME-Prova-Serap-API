e54d284135aaf2851399dbc1ccd9e8487c0fc244
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-12-01 12:29:55 -0300

    Update SincronizarQuestaoAlunoRespostaUseCase.cs

--- src/SME.SERAp.Prova.Aplicacao/UseCase/SincronizarQuestaoAlunoRespostaUseCase.cs
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/SincronizarQuestaoAlunoRespostaUseCase.cs
@@ -17,29 +17,7 @@ namespace SME.SERAp.Prova.Aplicacao
 
         public async Task<bool> Executar(QuestaoAlunoRespostaSincronizarDto dto)
         {
-            DateTime horaDataResposta = new(dto.DataHoraRespostaTicks);
-                        
-            var questaoRespondida = await mediator.Send(new ObterQuestaoAlunoRespostaPorIdRaQuery(dto.QuestaoId, dto.AlunoRa));
-
-            if (questaoRespondida == null)
-            {
-                return await mediator.Send(new IncluirQuestaoAlunoRespostaCommand(dto.QuestaoId, dto.AlunoRa, dto.AlternativaId, dto.Resposta, horaDataResposta, dto.TempoRespostaAluno ?? 0));
-            }
-            else if (questaoRespondida.CriadoEm > horaDataResposta)
-            {
-                return false;
-            }
-            else
-            {
-                questaoRespondida.Resposta = dto.Resposta;
-                questaoRespondida.AlternativaId = dto.AlternativaId;
-                questaoRespondida.TempoRespostaAluno += dto.TempoRespostaAluno ?? 0;
-                questaoRespondida.CriadoEm = horaDataResposta;
-                questaoRespondida.Visualizacoes += 1;
-
-                return await mediator.Send(new AtualizarQuestaoAlunoRespostaCommand(questaoRespondida));
-            }
-
+            return await mediator.Send(new PublicarFilaSerapEstudantesCommand(RotasRabbit.IncluirRespostaAluno, dto));
         }
     }
 }
