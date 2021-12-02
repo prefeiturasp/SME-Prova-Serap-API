fa1a59254af0839f591129e2e5f63b1702c1aad5
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-12-02 00:40:22 -0300

    Ajustando performance de claims para busca;

--- src/SME.SERAp.Prova.Aplicacao/UseCase/ObterProvasAreaEstudanteUseCase.cs
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/ObterProvasAreaEstudanteUseCase.cs
@@ -20,16 +20,18 @@ namespace SME.SERAp.Prova.Aplicacao
         public async Task<IEnumerable<ObterProvasRetornoDto>> Executar()
         {
 
-            //TODO: UNIFICAR BUSCA DAS CLAIMS
-            var alunoLogadoAno = await mediator.Send(new ObterUsuarioLogadoInformacaoPorClaimQuery("ANO"));
+            var claimsParaBuscar = new string[] { "ANO", "TIPOTURNO", "MODALIDADE", "RA" };
+            var claimsDoUsuario = await mediator.Send(new ObterUsuarioLogadoInformacoesPorClaimsQuery(claimsParaBuscar));
+
+            var alunoLogadoAno = claimsDoUsuario.FirstOrDefault(a => a.Chave == "ANO")?.Valor;
             if (string.IsNullOrEmpty(alunoLogadoAno))
                 throw new NegocioException("Ano do aluno logado não localizado");
 
-            var alunoLogadoTurno = await mediator.Send(new ObterUsuarioLogadoInformacaoPorClaimQuery("TIPOTURNO"));
+            var alunoLogadoTurno = claimsDoUsuario.FirstOrDefault(a => a.Chave == "TIPOTURNO")?.Valor;
             if (string.IsNullOrEmpty(alunoLogadoTurno))
                 throw new NegocioException("Turno do aluno logado não localizado");
 
-            var alunoLogadoModalidade = await mediator.Send(new ObterUsuarioLogadoInformacaoPorClaimQuery("MODALIDADE"));
+            var alunoLogadoModalidade = claimsDoUsuario.FirstOrDefault(a => a.Chave == "MODALIDADE")?.Valor;
             if (string.IsNullOrEmpty(alunoLogadoModalidade))
                 throw new NegocioException("Modalidade do aluno logado não localizado");
 
@@ -52,13 +54,16 @@ namespace SME.SERAp.Prova.Aplicacao
 
             if (provas.Any())
             {
-                var alunoRa = await mediator.Send(new ObterRAUsuarioLogadoQuery());
+                var alunoRa = claimsDoUsuario.FirstOrDefault(a => a.Chave == "RA")?.Valor;
+                
+                if (string.IsNullOrEmpty(alunoRa))
+                    throw new NegocioException("Não foi possível obter o RA do usuário logado.");
 
                 var provasParaRetornar = new List<ObterProvasRetornoDto>();
 
                 var provasIds = provas.Select(a => a.Id).Distinct().ToArray();
 
-                var provasDoAluno = await mediator.Send(new ObterProvaAlunoPorProvaIdsRaQuery(provasIds, alunoRa));
+                var provasDoAluno = await mediator.Send(new ObterProvaAlunoPorProvaIdsRaQuery(provasIds, long.Parse(alunoRa)));
 
                 foreach (var prova in provas)
                 {
