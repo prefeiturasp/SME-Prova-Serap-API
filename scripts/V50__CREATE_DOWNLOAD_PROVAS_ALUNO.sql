05dca181626302f6826fd26d2b46567b2470889f
Caique Siqueira <latorrecaique@gmail.com>
2021-12-13 17:25:51 -0300

    AB#54813

--- scripts/V50__CREATE_DOWNLOAD_PROVAS_ALUNO.sql
+++ scripts/V50__CREATE_DOWNLOAD_PROVAS_ALUNO.sql
@@ -2,16 +2,17 @@ create table if not exists public.downloads_prova_aluno
 (
 	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY ,
 	prova_id int8 NOT NULL,
-	aluno_id int8 NOT NULL,
+	aluno_ra int8 NOT NULL,
 	situacao int NOT NULL,
+	dispositivo_id varchar (200),
 	modelo_dispositivo varchar(200),
+	tipo_dispositivo int not null,
 	versao varchar (200),
 	criado_em timestamptz not null,
-	alterado_em timestamptz,
+	alterado_em timestamptz default null,
 	CONSTRAINT downloads_prova_aluno_id_pk PRIMARY KEY (id),
-	CONSTRAINT prova_id_fk FOREIGN KEY (prova_id) REFERENCES public.prova (id),
-	CONSTRAINT aluno_id_fk FOREIGN KEY (aluno_id) REFERENCES public.aluno (id)
+	CONSTRAINT prova_id_fk FOREIGN KEY (prova_id) REFERENCES public.prova (id)
 );
 
-create index if not exists pa_prova_idx ON public.downloads_prova_aluno(prova_id);
-create index if not exists pa_aluno_idx ON public.downloads_prova_aluno(aluno_id);
+create index if not exists pa_prova_idx ON public.downloads_prova_aluno using btree (prova_id);
+create index if not exists pa_aluno_idx ON public.downloads_prova_aluno   USING btree (aluno_ra);
