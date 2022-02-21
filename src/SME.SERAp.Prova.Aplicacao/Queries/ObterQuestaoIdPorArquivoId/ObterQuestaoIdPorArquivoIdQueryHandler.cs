37573b04f4b1ea1bb83c218c5976f85a8f8a2fa1
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-02-21 13:41:13 -0300

    ajustes

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterQuestaoIdPorArquivoId/ObterQuestaoIdPorArquivoIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dados;
+using SME.SERAp.Prova.Dominio;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ObterQuestaoIdPorArquivoIdQueryHandler : IRequestHandler<ObterQuestaoIdPorArquivoIdQuery, long>
+    {
+
+        private readonly IRepositorioQuestaoArquivo repositorioQuestaoArquivo;
+
+        public ObterQuestaoIdPorArquivoIdQueryHandler(IRepositorioQuestaoArquivo repositorioQuestaoArquivo)
+        {
+            this.repositorioQuestaoArquivo = repositorioQuestaoArquivo ?? throw new System.ArgumentNullException(nameof(repositorioQuestaoArquivo));
+        }
+
+        public async Task<long> Handle(ObterQuestaoIdPorArquivoIdQuery request, CancellationToken cancellationToken)
+        {
+            //return await repositorioQuestaoArquivo.ObterPorArquivoIdAsync(request.Id);
+        }
+    }
+}
