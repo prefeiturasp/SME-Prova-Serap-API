fc0a74c52f9ce16b37336c2e9d223ee20c718bab
Juliano de Campos Martins <juliano_campos_92@hotmail.com>
2022-03-01 14:39:25 -0400

    feat(AB#456): Listar todas as provas de todos os anos

---
+++ src/SME.SERAp.Prova.Infra/Dtos/PaginacaoResultadoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Infra.Dtos
+{
+    public class PaginacaoResultadoDto<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalPaginas { get; set; }
+        public int TotalRegistros { get; set; }
+
+        public bool Any()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
