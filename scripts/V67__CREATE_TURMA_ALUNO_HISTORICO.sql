9c61499b62a5fb49cca56a9136c8b93ae0da5847
Juliano de Campos Martins <juliano_campos_92@hotmail.com>
2022-03-11 16:49:04 -0400

    Ajuste script

--- scripts/V67__CREATE_TURMA_ALUNO_HISTORICO.sql
+++ scripts/V67__CREATE_TURMA_ALUNO_HISTORICO.sql
@@ -8,10 +8,6 @@ CREATE TABLE if not exists public.turma_aluno_historico (
 	data_matricula timestamp NULL,
 	data_situacao timestamp NULL,
 	CONSTRAINT turma_aluno_historico_pk PRIMARY KEY (id)
+	CONSTRAINT turma_aluno_historico_aluno_id_fk FOREIGN KEY (aluno_id) REFERENCES public.aluno(id),
+	CONSTRAINT turma_aluno_historico_turma_id_fk FOREIGN KEY (turma_id) REFERENCES public.turma (id)
 );
-
-
--- public.turma_aluno_historico foreign keys
-
-ALTER TABLE public.turma_aluno_historico ADD CONSTRAINT turma_aluno_historico_aluno_id_fk FOREIGN KEY (aluno_id) REFERENCES public.aluno(id);
-ALTER TABLE public.turma_aluno_historico ADD CONSTRAINT turma_aluno_historico_turma_id_fk FOREIGN KEY (turma_id) REFERENCES public.turma(id);
