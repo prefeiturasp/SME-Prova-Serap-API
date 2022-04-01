12e5985c88b837ff509db0db282e0c35ed1e8ada
Juliano de Campos Martins <juliano_campos_92@hotmail.com>
2022-04-01 12:22:40 -0400

    Correção serialização

--- src/SME.SERAp.Prova.Dominio/Entidades/EntidadeBase.cs
+++ src/SME.SERAp.Prova.Dominio/Entidades/EntidadeBase.cs
@@ -1,5 +1,8 @@
-﻿namespace SME.SERAp.Prova.Dominio
+﻿using MessagePack;
+
+namespace SME.SERAp.Prova.Dominio
 {
+    [MessagePackObject(keyAsPropertyName: true)]
     public abstract class EntidadeBase
     {
         public long Id { get; set; }
