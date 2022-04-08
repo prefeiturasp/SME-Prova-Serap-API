7f4ebaf17793ee10dee8d2135dee6c44ad4b3916
Juliano de Campos Martins <juliano_campos_92@hotmail.com>
2022-04-08 18:16:58 -0400

    removendo AppInsights

--- src/SME.SERAp.Prova.Dados/Cache/RepositorioCache.cs
+++ src/SME.SERAp.Prova.Dados/Cache/RepositorioCache.cs
@@ -1,69 +1,51 @@
 ﻿using MessagePack;
-using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.Memory;
 using SME.SERAp.Prova.Infra.Interfaces;
-using SME.SERAp.Prova.Infra.Utils;
 using StackExchange.Redis;
 using System;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SME.SERAp.Prova.Dados.Cache
 {
     public class RepositorioCache : IRepositorioCache
     {
-
         private readonly IServicoLog servicoLog;
-        private readonly IConnectionMultiplexer distributedCache;
-
-        public RepositorioCache(IServicoLog servicoLog, IConnectionMultiplexer distributedCache)
+        private readonly IDatabase database;
+        public RepositorioCache(IServicoLog servicoLog, IConnectionMultiplexer connectionMultiplexer)
         {
             this.servicoLog = servicoLog ?? throw new ArgumentNullException(nameof(servicoLog));
-            this.distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+            if (connectionMultiplexer == null) throw new ArgumentNullException(nameof(connectionMultiplexer));
+            this.database = connectionMultiplexer.GetDatabase();
         }
 
         public async Task SalvarRedisAsync(string nomeChave, object valor, int minutosParaExpirar = 720)
         {
-            var inicioOperacao = DateTime.UtcNow;
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-
-            var db = distributedCache.GetDatabase();
-            await db.StringSetAsync(nomeChave, MessagePackSerializer.Serialize(valor), TimeSpan.FromMinutes(minutosParaExpirar));
-
-            timer.Stop();
-            servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Salvar Redis Async", inicioOperacao, timer.Elapsed, true);
+            try
+            {
+                await database.StringSetAsync(nomeChave, MessagePackSerializer.Serialize(valor), TimeSpan.FromMinutes(minutosParaExpirar));
+            }
+            catch(Exception ex)
+            {
+                servicoLog.Registrar(ex);
+            }
         }
 
         public async Task RemoverRedisAsync(string nomeChave)
         {
-            var inicioOperacao = DateTime.UtcNow;
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-
             try
             {
-                var db = distributedCache.GetDatabase();
-                await db.KeyDeleteAsync(nomeChave);
-                timer.Stop();
+                await database.KeyDeleteAsync(nomeChave);
             }
             catch (Exception ex)
             {
-                timer.Stop();
                 servicoLog.Registrar(ex);
             }
         }
 
         public async Task<T> ObterRedisAsync<T>(string nomeChave, Func<Task<T>> buscarDados, int minutosParaExpirar = 720)
         {
-            var inicioOperacao = DateTime.UtcNow;
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-
             try
             {
-                var db = distributedCache.GetDatabase();
-                byte[] byteCache = await db.StringGetAsync(nomeChave);
-
-                timer.Stop();
-                servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Obtendo", inicioOperacao, timer.Elapsed, true);
+                byte[] byteCache = await database.StringGetAsync(nomeChave);
 
                 if (byteCache != null)
                 {
@@ -78,9 +60,6 @@ namespace SME.SERAp.Prova.Dados.Cache
             catch (Exception ex)
             {
                 servicoLog.Registrar(ex);
-                timer.Stop();
-
-                servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, $"Obtendo - Erro {ex.Message}", inicioOperacao, timer.Elapsed, false);
             }
 
             return default;
@@ -88,16 +67,9 @@ namespace SME.SERAp.Prova.Dados.Cache
 
         public async Task<T> ObterRedisAsync<T>(string nomeChave)
         {
-            var inicioOperacao = DateTime.UtcNow;
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-
             try
             {
-                var db = distributedCache.GetDatabase();
-                byte[] byteCache = await db.StringGetAsync(nomeChave);
-
-                timer.Stop();
-                servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, "Obtendo", inicioOperacao, timer.Elapsed, true);
+                byte[] byteCache = await database.StringGetAsync(nomeChave);
 
                 if (byteCache != null)
                 {
@@ -107,9 +79,6 @@ namespace SME.SERAp.Prova.Dados.Cache
             catch (Exception ex)
             {
                 servicoLog.Registrar(ex);
-                timer.Stop();
-
-                servicoLog.RegistrarDependenciaAppInsights("Redis", nomeChave, $"Obtendo - Erro {ex.Message}", inicioOperacao, timer.Elapsed, false);
             }
 
             return default;
