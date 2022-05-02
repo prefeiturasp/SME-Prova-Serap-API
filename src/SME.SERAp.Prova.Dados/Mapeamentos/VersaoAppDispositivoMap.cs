ea4458e6117621fc78f3bbdc90924eefdd926cb8
Juliano Martins <juliano_campos_92@hotmail.com>
2022-05-02 15:30:37 -0400

    Task 63371: Criar endpoint para armazenar versão do dispositivo

---
+++ src/SME.SERAp.Prova.Dados/Mapeamentos/VersaoAppDispositivoMap.cs
@@ -0,0 +1,18 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+
+namespace SME.SERAp.Prova.Dados
+{
+    public  class VersaoAppDispositivoMap : DommelEntityMap<Dominio.VersaoAppDispositivo>
+    {
+        public VersaoAppDispositivoMap()
+        {
+            ToTable("versao_app_dispositivo");
+            Map(c => c.Id).ToColumn("id").IsKey();
+            Map(c => c.VersaoCodigo).ToColumn("codigo_versao");
+            Map(c => c.VersaoDescricao).ToColumn("descricao_versao");
+            Map(c => c.DispositivoImei).ToColumn("dispositivo_imei");
+            Map(c => c.AtualizadoEm).ToColumn("atualizado_em");
+            Map(c => c.CriadoEm).ToColumn("criado_em");
+        }
+    }
+}
---
+++ src/SME.SERAp.Prova.Infra/Dtos/Versao/VersaoAppDispositivoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SME.SERAp.Prova.Infra
+{
+    public class VersaoAppDispositivoDto : DtoBase
+    {
+        public int VersaoCodigo { get; set; }
+        public string VersaoDescricao { get; set; }
+        public string DispositivoImei { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+    }
+}
