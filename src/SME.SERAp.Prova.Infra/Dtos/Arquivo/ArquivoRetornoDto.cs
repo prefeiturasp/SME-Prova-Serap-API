1834d33122b8a22ea2523d5357661e99d7feb9c5
Caique Siqueira <latorrecaique@gmail.com>
2022-06-07 17:37:48 -0300

    AB#66576

--- src/SME.SERAp.Prova.Infra/Dtos/Arquivo/ArquivoRetornoDto.cs
+++ src/SME.SERAp.Prova.Infra/Dtos/Arquivo/ArquivoRetornoDto.cs
@@ -4,23 +4,25 @@
     {
         public ArquivoRetornoDto()
         {
-
         }
 
         public ArquivoRetornoDto(long id, string caminho)
         {
             Id = id;
+            LegadoId = id;
             Caminho = caminho;
         }
 
         public ArquivoRetornoDto(long id, string caminho, long questaoId)
         {
             Id = id;
+            LegadoId = id;
             Caminho = caminho;
             QuestaoId = questaoId;
         }
 
         public long Id { get; set; }
+        public long LegadoId { get; set; }
         public string Caminho { get; set; }
         public long QuestaoId { get; set; }
     }
