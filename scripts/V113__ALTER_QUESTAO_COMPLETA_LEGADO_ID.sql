26b1a62c043e801ddc7f6a88cddc2745e4b72fcb
Juliano Martins <juliano_campos_92@hotmail.com>
2022-07-19 09:04:32 -0400

    ajuste

--- scripts/V113__ALTER_QUESTAO_COMPLETA_LEGADO_ID.sql
+++ scripts/V113__ALTER_QUESTAO_COMPLETA_LEGADO_ID.sql
@@ -1,2 +1,2 @@
 ALTER TABLE questao_completa ADD questao_legado_id int8 NULL;
-update questao_completa set ultima_atualizacao = null where ultima_atualizacao is not null;
+update questao_completa set ultima_atualizacao = '2019-01-01 00:00:00' where ultima_atualizacao is not null;
