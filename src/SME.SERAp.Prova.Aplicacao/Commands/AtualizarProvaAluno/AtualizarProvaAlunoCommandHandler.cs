f2e90b1b95e25e4fe3ab8e3707511886b9403b18
Juliano Martins <juliano_campos_92@hotmail.com>
2022-09-13 14:22:03 -0400

    User Story 72231: [SERAp Estudantes] Validar data e hora do dispositivo com o servidor - G

--- src/SME.SERAp.Prova.Aplicacao/Commands/AtualizarProvaAluno/AtualizarProvaAlunoCommandHandler.cs
+++ src/SME.SERAp.Prova.Aplicacao/Commands/AtualizarProvaAluno/AtualizarProvaAlunoCommandHandler.cs
@@ -19,8 +19,14 @@ namespace SME.SERAp.Prova.Aplicacao
             this.repositorioCache = repositorioCache ?? throw new System.ArgumentNullException(nameof(repositorioCache));
 
         }
+
         public async Task<bool> Handle(AtualizarProvaAlunoCommand request, CancellationToken cancellationToken)
         {
+            if(request.ProvaAluno.Status == ProvaStatus.Finalizado)
+            {
+                request.ProvaAluno.FinalizadoEmServidor = DateTime.Now;
+            }
+
             string chaveProvaAluno = string.Format(CacheChave.AlunoProva, request.ProvaAluno.ProvaId, request.ProvaAluno.AlunoRA);
             await mediator.Send(new PublicarFilaSerapEstudantesCommand(RotasRabbit.IncluirProvaAluno, request.ProvaAluno));
             await repositorioCache.SalvarRedisAsync(chaveProvaAluno, request.ProvaAluno);
