62c9991b51f85b6c97f6d8389e33804d3256dd83
Caique Siqueira <latorrecaique@gmail.com>
2022-09-22 16:34:08 -0300

    AB#74667 Ajuste script tabela de log de reabertura

--- scripts/V116__CREATE_ALUNO_PROVA_REABERTURA.sql
+++ scripts/V116__CREATE_ALUNO_PROVA_REABERTURA.sql
@@ -1,14 +1,13 @@
-create table if not exists public.prova_aluno_reabertura
-(
+CREATE TABLE IF NOT EXISTS public.prova_aluno_reabertura (
 	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
-	prova_id bigint not null,
-	login_serap varchar(60) not null,
-	grupo_serap varchar(500) not null,
-	aluno_ra  int8 not null,
-	criado_em timestamp without time zone not null,
-	alterado_em timestamp without time zone, 
+	prova_id int8 NOT NULL,
+	login_coresso varchar(60) NOT NULL,
+	usuario_id_coresso uuid NOT NULL,
+	grupo_coresso uuid NOT NULL,
+	aluno_ra int8 NOT NULL,
+	criado_em timestamp NOT NULL,
+	alterado_em timestamp NULL,
 	CONSTRAINT reabertura_pk PRIMARY KEY (id)
 );
 
-ALTER TABLE public.prova_aluno_reabertura ADD CONSTRAINT prova_id_fk FOREIGN KEY (prova_id) REFERENCES prova(id);
-
+ALTER TABLE public.prova_aluno_reabertura ADD CONSTRAINT prova_id_fk FOREIGN KEY (prova_id) REFERENCES public.prova(id);
