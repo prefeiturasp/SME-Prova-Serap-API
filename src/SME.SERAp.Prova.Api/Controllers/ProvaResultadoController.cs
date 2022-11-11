d840454611524dd66899ad479761e67fc464d70f
krisamcom <krishnamuti.barros@amcom.com.br>
2022-11-11 16:15:09 -0300

    story:76142 - estrutura end point resultado prova resumo e campo alternativa correta

--- src/SME.SERAp.Prova.Api/Controllers/ProvaResultadoController.cs
+++ src/SME.SERAp.Prova.Api/Controllers/ProvaResultadoController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SME.SERAp.Prova.Api.Middlewares;
 using SME.SERAp.Prova.Aplicacao;
 using SME.SERAp.Prova.Infra;
-using SME.SERAp.Prova.Infra.Dtos.Prova;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -18,13 +16,13 @@ namespace SME.SERAp.Prova.Api.Controllers
 
         }
 
-        //[HttpGet("{provaId}/{caderno}/resumo")]
-        //[ProducesResponseType(typeof(IEnumerable<ObterProvasAnterioresRetornoDto>), 200)]
-        //[ProducesResponseType(typeof(RetornoBaseDto), 500)]
-        //[Authorize("Bearer")]
-        //public async Task<IActionResult> ObterProvasAnteriores([FromServices] IObterProvasAnterioresAreaEstudanteUseCase obterProvasAnterioresAreaEstudanteUseCase)
-        //{
-        //    return Ok(await obterProvasAnterioresAreaEstudanteUseCase.Executar());
-        //}
+        [HttpGet("{provaId}/{caderno}/resumo")]
+        [ProducesResponseType(typeof(IEnumerable<ProvaResultadoResumoDto>), 200)]
+        [ProducesResponseType(typeof(RetornoBaseDto), 500)]
+        [Authorize("Bearer")]
+        public async Task<IActionResult> ObterProvasAnteriores(long provaId, int caderno, [FromServices] IObterProvaResultadoResumoUseCase obterProvaResultadoResumoUseCase)
+        {
+            return Ok(await obterProvaResultadoResumoUseCase.Executar(provaId, caderno));
+        }
     }
 }
---
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/ObterProvaResultadoResumoUseCase.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Infra;
+using SME.SERAp.Prova.Infra.Exceptions;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Aplicacao.UseCase
+{
+    public class ObterProvaResultadoResumoUseCase : IObterProvaResultadoResumoUseCase
+    {
+
+        private readonly IMediator mediator;
+
+        public ObterProvaResultadoResumoUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<ProvaResultadoResumoDto> Executar(long provaId, int caderno)
+        {
+            return default;
+        }
+    }
+}
