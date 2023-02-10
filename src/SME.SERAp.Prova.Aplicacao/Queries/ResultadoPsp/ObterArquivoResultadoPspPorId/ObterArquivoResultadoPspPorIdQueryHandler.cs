02bef75e329ce5a813ab4df22af41f935d672153
krisamcom <krishnamuti.barros@amcom.com.br>
2023-02-10 17:48:23 -0300

    bug: 86064 - baixar arquivo servidor

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ResultadoPsp/ObterArquivoResultadoPspPorId/ObterArquivoResultadoPspPorIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dados.Interfaces;
+using SME.SERAp.Prova.Infra;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    internal class ObterArquivoResultadoPspPorIdQueryHandler : IRequestHandler<ObterArquivoResultadoPspPorIdQuery, ArquivoResultadoPspDto>
+    {
+
+        private readonly IRepositorioArquivoResultadoPsp repositorioArquivoResultadoPsp;
+
+        public ObterArquivoResultadoPspPorIdQueryHandler(IRepositorioArquivoResultadoPsp repositorioArquivoResultadoPsp)
+        {
+            this.repositorioArquivoResultadoPsp = repositorioArquivoResultadoPsp ?? throw new System.ArgumentNullException(nameof(repositorioArquivoResultadoPsp));
+        }
+
+        public async Task<ArquivoResultadoPspDto> Handle(ObterArquivoResultadoPspPorIdQuery request, CancellationToken cancellationToken)
+        {
+            return await repositorioArquivoResultadoPsp.ObterArquivoResultadoPspPorId(request.Id);
+        }
+    }
+}
