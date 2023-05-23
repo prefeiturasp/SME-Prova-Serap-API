3ce8249af625a5fe8e7d6261d511b789cd20cd5f
krisamcom <krishnamuti.barros@amcom.com.br>
2023-05-23 18:48:03 -0300

    story:93750 - endpoint obter data ultima sincronização provas

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterUltimaExecucaoControleTipoPorTipo/ObterUltimaExecucaoControleTipoPorTipoQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dominio;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ObterUltimaExecucaoControleTipoPorTipoQuery : IRequest<ExecucaoControle>
+    {
+        public ObterUltimaExecucaoControleTipoPorTipoQuery(ExecucaoControleTipo tipo)
+        {
+            Tipo = tipo;
+        }
+
+        public ExecucaoControleTipo Tipo { get; set; }
+    }
+}
