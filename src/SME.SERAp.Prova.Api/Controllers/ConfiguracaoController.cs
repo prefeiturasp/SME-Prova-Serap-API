2f6174d0466bca19f92173f65b0da3c7b0a0aa3a
fernando.groeler <fernando.groeler@gmail.com>
2023-10-19 15:12:45 -0400

    Criado endpoint para verificar conexão.

--- src/SME.SERAp.Prova.Api/Controllers/ConfiguracaoController.cs
+++ src/SME.SERAp.Prova.Api/Controllers/ConfiguracaoController.cs
@@ -28,6 +28,12 @@ namespace SME.SERAp.Prova.Api.Controllers
         {
             return Ok(await obterDataHoraServidorUseCase.Executar());
         }
-
+        
+        [HttpGet("existe-conexao")]
+        [ProducesResponseType(typeof(bool), 200)]
+        public async Task<IActionResult> VerificaConexao()
+        {
+            return Ok(await Task.FromResult(true));
+        }
     }
 }
