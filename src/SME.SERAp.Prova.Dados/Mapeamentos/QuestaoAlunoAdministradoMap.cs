d09d1bd752fedbf264ed4c4806ab310997cf21ab
fernando.groeler <fernando.groeler@gmail.com>
2024-01-08 17:04:13 -0400

    Adicionado mapeamento.

---
+++ src/SME.SERAp.Prova.Dados/Mapeamentos/QuestaoAlunoAdministradoMap.cs
@@ -0,0 +1,18 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using SME.SERAp.Prova.Dominio;
+
+namespace SME.SERAp.Prova.Dados
+{
+    public class QuestaoAlunoAdministradoMap : DommelEntityMap<QuestaoAlunoAdministrado>
+    {
+        public QuestaoAlunoAdministradoMap()
+        {
+            ToTable("questao_aluno_administrado");
+            Map(c => c.Id).ToColumn("id").IsKey();
+            Map(c => c.QuestaoId).ToColumn("questao_id");
+            Map(c => c.AlunoId).ToColumn("aluno_id");
+            Map(c => c.Ordem).ToColumn("ordem");
+            Map(c => c.CriadoEm).ToColumn("criado_em");            
+        }
+    }
+}
