9027caae4e62a7e147f6529d5b986a1211949a92
wesleygyn <wesley_gyn@yahoo.com.br>
2025-05-27 10:59:40 -0300

    #128723 Correção do problema

--- src/SME.SERAp.Prova.Aplicacao/UseCase/SincronizarQuestaoAlunoRespostaUseCase.cs
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/SincronizarQuestaoAlunoRespostaUseCase.cs
@@ -18,27 +18,37 @@ namespace SME.SERAp.Prova.Aplicacao
 
         public async Task<bool> Executar(List<QuestaoAlunoRespostaSincronizarDto> listaAlunoResposta)
         {
-            foreach(var dto in listaAlunoResposta)
+            foreach (var dto in listaAlunoResposta)
             {
                 var provas = await mediator.Send(new ObterProvasEmAndamentoPorQuestaoIdQuery(dto.QuestaoId));
 
                 foreach (var prova in provas)
                 {
-                    var questoesAlternativas = await mediator.Send(new ObterAlternativaAlunoRespostaQuery(prova.ProvaId, dto.AlunoRa));
-
-                    var questao = questoesAlternativas.FirstOrDefault(c => c.QuestaoId == dto.QuestaoId);
-                    if (questao == null)
+                    //-> Verifica se a prova é TAI ou BIB e se a questão é nula para executar o "continue"
+                    if (await ValidarQuestaoNaoExisteProvaAluno(prova.ProvaId, dto.AlunoRa, dto.QuestaoId))
                         continue;
-                    
+                    //-> Publica resposta
                     await mediator.Send(new PublicarFilaSerapEstudantesCommand(RotasRabbit.IncluirRespostaAluno, dto));
-                    
-                    //-> Acompanhamento
+
                     var dtoAcompanhamento = new QuestaoAlunoRespostaAcompDto(0, dto.AlunoRa, dto.QuestaoId, dto.AlternativaId, dto.TempoRespostaAluno);
+                    //-> Publica acompanhamento
                     await mediator.Send(new PublicarFilaSerapEstudanteAcompanhamentoCommand(RotasRabbit.AcompProvaAlunoRespostaTratar, dtoAcompanhamento));
                 }
             }
 
             return true;
+        }
+
+        private async Task<bool> ValidarQuestaoNaoExisteProvaAluno(long provaId, long alunoRa, long questaoId)
+        {
+            var prova = await mediator.Send(new ObterProvaPorIdQuery(provaId));
+
+            if (!prova.PossuiBIB && !prova.FormatoTai)
+                return false;
+
+            var questoesAlternativas = await mediator.Send(new ObterAlternativaAlunoRespostaQuery(provaId, alunoRa));
+            var questao = questoesAlternativas.FirstOrDefault(c => c.QuestaoId == questaoId);
+            return questao == null;
         }
     }
 }
