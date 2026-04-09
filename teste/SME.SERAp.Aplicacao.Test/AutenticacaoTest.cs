d1a50ef6841ecaa239b2f6210eb16d6a6c52566d
Mario Junior <mariosj9@gmail.com>
2026-04-09 15:11:53 -0300

    Ajuste AutenticacaoTest

--- teste/SME.SERAp.Aplicacao.Test/AutenticacaoTest.cs
+++ teste/SME.SERAp.Aplicacao.Test/AutenticacaoTest.cs
@@ -10,13 +10,14 @@ namespace SME.SERAp.Aplicacao.Test
     public class AutenticacaoTest
     {
         public static IEnumerable<object[]> VerificaRegraSenhaData =>
-        [
-            [5215402, "01022010", new DateTime(2010, 01, 01, 0, 0, 0), false],
-            [5215402, "20042012", new DateTime(2012, 04, 20, 0, 0, 0), true],
-            [5215402, "10052010", new DateTime(2009, 05, 09, 0, 0, 0), false],
-            [856472584, "02112015", new DateTime(2015, 11, 02, 0, 0, 0), true],
-            [856472584, "16072016", new DateTime(2016, 07, 15, 0, 0, 0), false],
-        ];
+            new List<object[]>
+            {
+                new object[] { 5215402, "01022010", new DateTime(2010, 01, 01, 0, 0, 0), false },
+                new object[] { 5215402, "20042012", new DateTime(2012, 04, 20, 0, 0, 0), true },
+                new object[] { 5215402, "10052010", new DateTime(2009, 05, 09, 0, 0, 0), false },
+                new object[] { 856472584, "02112015", new DateTime(2015, 11, 02, 0, 0, 0), true },
+                new object[] { 856472584, "16072016", new DateTime(2016, 07, 15, 0, 0, 0), false }
+            };
 
         [Theory]
         [MemberData(nameof(VerificaRegraSenhaData))]
